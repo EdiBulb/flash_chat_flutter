2b1c50c749306e71f61e9dd82710ca67972a7b92
Harry <89003206+EdiBulb@users.noreply.github.com>
2025-05-02 07:35:31 +1200

    ListView added. TextEditingController() added. StreamBuilder refactored into MessagesStream class.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,8 +1,10 @@
-import 'package:flash_chat_flutter/screens/login_screen.dart';
-import 'package:flutter/material.dart';
-import 'package:flash_chat_flutter/constants.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flash_chat_flutter/constants.dart';
+import 'package:flutter/material.dart';
+
+final _firestore = FirebaseFirestore.instance; // 텍스트 DB에 저장하기 위함
+
 
 class ChatScreen extends StatefulWidget {
   static const String id = 'chat_screen'; // routes typo 예방
@@ -12,8 +14,9 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final messageTextController = TextEditingController(); // 텍스트 순서 맞추기 위함
   final _firestore = FirebaseFirestore.instance; // 텍스트 DB에 저장하기 위함
-  final _auth= FirebaseAuth.instance;
+  final _auth = FirebaseAuth.instance;
   late User loggedInUser;
   String? messageText;
 
@@ -30,10 +33,11 @@ class _ChatScreenState extends State<ChatScreen> {
         loggedInUser = user;
         // print(loggedInUser.email); // 로그인 된 유저의 이메일 정보를 print한다.
       }
-    } catch(e) {
+    } catch (e) {
       print(e);
     }
   }
+
   // void getMessages() async {
   //   final messages = await _firestore.collection('messages').get(); // messages를 db에서 가져오는 함수
   //   //messages 데이터의 value를 확인
@@ -45,7 +49,7 @@ class _ChatScreenState extends State<ChatScreen> {
   // 데이터를 1번만 가져오는 일반요청인 get과 다르게 stream으로 실시간 데이터 처리함.
   // getMessages()를 쓰지않고 Stream을 쓴다. because Stream을 쓰면 데이터가 변경될 때마다 자동으로 push됨, 내가 firestore에서 pull하지 않아도 됌.
   void messagesStream() async {
-    await for( var snapshot in _firestore.collection('messages').snapshots()){
+    await for (var snapshot in _firestore.collection('messages').snapshots()) {
       for (var message in snapshot.docs) {
         print(message.data());
       }
@@ -59,13 +63,14 @@ class _ChatScreenState extends State<ChatScreen> {
         leading: null,
         actions: <Widget>[
           IconButton(
-              icon: Icon(Icons.close),
-              onPressed: () {
-                //Implement logout functionality
-                // _auth.signOut();
-                // Navigator.pop(context);
-                messagesStream();
-              }),
+            icon: Icon(Icons.close),
+            onPressed: () {
+              //Implement logout functionality
+              // _auth.signOut();
+              // Navigator.pop(context);
+              messagesStream();
+            },
+          ),
         ],
         title: Text('⚡️Chat'),
         backgroundColor: Colors.lightBlueAccent,
@@ -75,35 +80,7 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
-            StreamBuilder<QuerySnapshot>( // StreamBuilder 부분 어려움
-                stream: _firestore.collection('messages').snapshots(),
-                builder: (context, snapshot){
-                  // 스냅샷에 데이터가 없을 경우
-                  if (!snapshot.hasData) {
-                    return Center(
-                      child: CircularProgressIndicator(
-                        backgroundColor: Colors.lightBlueAccent,
-                      ),
-                    );
-                  }
-                    final messages = snapshot.data!.docs;
-                    List<Text> messageWidgets = [];
-                    for (var message in messages) {
-                      final messageData = message.data() as Map<String, dynamic>;
-                      final messageText = messageData['text'];
-                      final messageSender = messageData['sender'];
-
-                      final messageWidget = Text('$messageText from $messageSender');
-                      messageWidgets.add(messageWidget);
-                    }
-                    return Column(
-                      children: messageWidgets,
-                    );
-
-                  return Center(child: CircularProgressIndicator()); // 에러나서 추가했음 일단.
-
-                },
-            ),
+            MessagesStream(),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
@@ -111,7 +88,9 @@ class _ChatScreenState extends State<ChatScreen> {
                 children: <Widget>[
                   Expanded(
                     child: TextField(
-                      onChanged: (value) { // 입력창에 입력된 글자를 실시간으로 콜백하는 함수
+                      controller: messageTextController, // 이제 TextField를 컨트롤할 수 있다.
+                      onChanged: (value) {
+                        // 입력창에 입력된 글자를 실시간으로 콜백하는 함수
                         //Do something with the user input.
                         messageText = value;
                       },
@@ -120,22 +99,104 @@ class _ChatScreenState extends State<ChatScreen> {
                   ),
                   TextButton(
                     onPressed: () {
+                      messageTextController.clear(); // 입력하면 기존 것 클리어됨
                       //Implement send functionality.
-                      _firestore.collection('messages').add({ // map 형태임
+                      _firestore.collection('messages').add({
+                        // map 형태임
                         'text': messageText,
                         'sender': loggedInUser.email,
                       });
                     },
-                    child: Text(
-                      'Send',
-                      style: kSendButtonTextStyle,
-                    ),
+                    child: Text('Send', style: kSendButtonTextStyle),
                   ),
                 ],
               ),
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class MessagesStream extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder<QuerySnapshot>(
+      // StreamBuilder 부분 어려움
+      stream: _firestore.collection('messages').snapshots(),
+      builder: (context, snapshot) {
+        // 스냅샷에 데이터가 없을 경우
+        if (!snapshot.hasData) {
+          return Center(
+            child: CircularProgressIndicator(
+              backgroundColor: Colors.lightBlueAccent,
+            ),
+          );
+        }
+        final messages = snapshot.data!.docs;
+        List<MessageBubble> messageBubbles = [];
+        for (var message in messages) {
+          final messageData = message.data() as Map<String, dynamic>;
+          final messageText = messageData['text'];
+          final messageSender = messageData['sender'];
+
+          final messageBubble = MessageBubble(
+            sender: messageSender,
+            text: messageText,
+          );
+          messageBubbles.add(messageBubble);
+        }
+        return Expanded(
+          child: ListView(
+            // ListView
+            padding: EdgeInsets.symmetric(
+              horizontal: 10.0,
+              vertical: 20.0,
+            ),
+            children: messageBubbles,
+          ),
+        );
+
+        return Center(
+          child: CircularProgressIndicator(),
+        ); // 에러나서 추가했음 일단.
+      },
+    );
+  }
+}
+
+
+// 메세지 디자인
+class MessageBubble extends StatelessWidget {
+  MessageBubble({this.sender, this.text});
+
+  final String? sender;
+  final String? text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end, // 오른쪽 배치
+        children: [
+          Text(sender!,
+          style: TextStyle(fontSize: 12.0, color: Colors.black54),),
+          Material(
+            borderRadius: BorderRadius.circular(30.0),
+            elevation: 5.0,
+            color: Colors.lightBlueAccent,
+            child: Padding(
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+              child: Text(
+                text!,
+                style: TextStyle(color: Colors.white, fontSize: 15.0),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
