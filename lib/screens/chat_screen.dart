5bf4a4bd3c21c88a9a1c83e9fbda857d16d7a6a0
Harry <89003206+EdiBulb@users.noreply.github.com>
2025-05-01 17:31:50 +1200

    Learned about stream. added StreamBuilder.

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -33,8 +33,25 @@ class _ChatScreenState extends State<ChatScreen> {
     } catch(e) {
       print(e);
     }
-
   }
+  // void getMessages() async {
+  //   final messages = await _firestore.collection('messages').get(); // messages를 db에서 가져오는 함수
+  //   //messages 데이터의 value를 확인
+  //   for (var message in messages.docs){
+  //     print(message.data());
+  //   }
+  // }
+
+  // 데이터를 1번만 가져오는 일반요청인 get과 다르게 stream으로 실시간 데이터 처리함.
+  // getMessages()를 쓰지않고 Stream을 쓴다. because Stream을 쓰면 데이터가 변경될 때마다 자동으로 push됨, 내가 firestore에서 pull하지 않아도 됌.
+  void messagesStream() async {
+    await for( var snapshot in _firestore.collection('messages').snapshots()){
+      for (var message in snapshot.docs) {
+        print(message.data());
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -45,8 +62,9 @@ class _ChatScreenState extends State<ChatScreen> {
               icon: Icon(Icons.close),
               onPressed: () {
                 //Implement logout functionality
-                _auth.signOut();
-                Navigator.pop(context);
+                // _auth.signOut();
+                // Navigator.pop(context);
+                messagesStream();
               }),
         ],
         title: Text('⚡️Chat'),
@@ -57,6 +75,35 @@ class _ChatScreenState extends State<ChatScreen> {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: <Widget>[
+            StreamBuilder<QuerySnapshot>( // StreamBuilder 부분 어려움
+                stream: _firestore.collection('messages').snapshots(),
+                builder: (context, snapshot){
+                  // 스냅샷에 데이터가 없을 경우
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: CircularProgressIndicator(
+                        backgroundColor: Colors.lightBlueAccent,
+                      ),
+                    );
+                  }
+                    final messages = snapshot.data!.docs;
+                    List<Text> messageWidgets = [];
+                    for (var message in messages) {
+                      final messageData = message.data() as Map<String, dynamic>;
+                      final messageText = messageData['text'];
+                      final messageSender = messageData['sender'];
+
+                      final messageWidget = Text('$messageText from $messageSender');
+                      messageWidgets.add(messageWidget);
+                    }
+                    return Column(
+                      children: messageWidgets,
+                    );
+
+                  return Center(child: CircularProgressIndicator()); // 에러나서 추가했음 일단.
+
+                },
+            ),
             Container(
               decoration: kMessageContainerDecoration,
               child: Row(
